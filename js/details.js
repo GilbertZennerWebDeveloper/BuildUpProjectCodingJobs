bcac36acb66faf2a0bd39e0bb744d3d05e0497f2
Nataly Kuzminska <kuzminskanataly@gmail.com>
2022-10-05 14:46:15 +0300

    create detailes.js

--- js/details.js
+++ js/details.js
@@ -1,13 +1,17 @@
 const container = document.querySelector(".container");
-const img = document.querySelector("img");
+const img = document.querySelectorAll("img");
 const animeName = document.querySelector(".animename");
 const animeYear = document.querySelector(".animeyear");
 const animeDuration = document.querySelector(".animeduration");
+const moredetails = document.querySelectorAll(".moredetails");
+let detailsMovieTitle;
 
-fetch(`https://api.jikan.moe/v4/anime?q=naruto ${input.value}`)
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  });
+moredetails.addEventListener("click", () => {
+  fetch(`https://api.jikan.moe/v4/anime?q=${value}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    });
+});
